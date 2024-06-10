e550f2dec76881d250f30bf15fb339745002dd9a
TommasoBi <tommaso.bianchi7@edu.unifi.it>
2024-06-10 12:02:50 +0200

    scaricato googletest + implementato test

---
+++ tests/test_main.cpp
@@ -0,0 +1,29 @@
+
+#include "../src/BankAccount.h"
+#include "../src/Income.h"
+#include "../src/Expense.h"
+#include "googletest/googletest/include/gtest/gtest.h"
+
+
+TEST(BankAccountTest, InitialBalanceIsZero) {
+    BankAccount account;
+    EXPECT_EQ(account.getBalance(), 0.0);
+}
+
+TEST(BankAccountTest, AddIncomeIncreasesBalance) {
+    BankAccount account;
+    account.addTransaction(std::make_shared<Income>(100.0, "2024-06-08", "Salary"));
+    EXPECT_EQ(account.getBalance(), 100.0);
+}
+
+TEST(BankAccountTest, AddExpenseDecreasesBalance) {
+    BankAccount account;
+    account.addTransaction(std::make_shared<Income>(100.0, "2024-06-08", "Salary"));
+    account.addTransaction(std::make_shared<Expense>(50.0, "2024-06-09", "Groceries"));
+    EXPECT_EQ(account.getBalance(), 50.0);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
